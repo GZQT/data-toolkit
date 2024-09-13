2544014458f5285c296bc01dbb6af189acb57e07
李中月 <lizhongyue248@163.com>
2024-09-13 15:13:20 +0800

    🔖 1.0.4 版本

--- client/quasar.config.ts
+++ client/quasar.config.ts
@@ -221,7 +221,7 @@ export default configure((/* ctx */) => {
         appId: 'data-toolkit',
         productName: 'Data Toolkit',
         releaseInfo: {
-          releaseNotesFile: 'changelog/CHANGELOG_1.0.1.md'
+          releaseNotesFile: 'changelog/CHANGELOG_1.0.4.md'
         },
         extraResources: [
           {
